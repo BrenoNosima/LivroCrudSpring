673efd73aa77eaf4ea12a0a440d0636fdec4f6b7
Nosima <breno.nosima@gmail.com>
2026-03-11 19:45:30 -0300

    first commit

---
+++ src/main/java/com/bn/atvdjava/AtvdJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bn.atvdjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvdJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtvdJavaApplication.class, args);
+    }
+
+}
